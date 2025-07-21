0e20a4830712565133e216a504577eaa214846e9
Vadim Dzgoev <vadim-bmk@yandex.ru>
2025-07-21 14:30:37 +0300

    Initial commit

---
+++ src/main/kotlin/com/dvo/taskManagerPro/TaskManagerProApplication.kt
@@ -0,0 +1,11 @@
+package com.dvo.taskManagerPro
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaskManagerProApplication
+
+fun main(args: Array<String>) {
+	runApplication<TaskManagerProApplication>(*args)
+}
---
+++ src/test/kotlin/com/dvo/taskManagerPro/TaskManagerProApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dvo.taskManagerPro
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaskManagerProApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
